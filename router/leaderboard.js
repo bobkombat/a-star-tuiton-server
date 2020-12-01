00d8b9c5087b909dee94bd23346cf5153f90ee75
bobkombat <rdhwnrmdhny@gmail.com>
2020-12-01 11:56:34 +0700

    add: authentication

--- router/leaderboard.js
+++ router/leaderboard.js
@@ -1,7 +1,10 @@
 const router = require("express").Router();
 const LeaderboardController = require("../controllers/leaderboardController");
+const authentication = require("../middlewares/authentication");
 
 router.get("/all", LeaderboardController.getAll);
+
+router.use(authentication);
 router.get("/view-detail", LeaderboardController.getOneUser);
 
 module.exports = router;
