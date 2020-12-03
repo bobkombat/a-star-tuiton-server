2a686289725daf4a3ffb99db7f977ec25a094a7d
bobkombat <rdhwnrmdhny@gmail.com>
2020-12-03 10:46:36 +0700

    fix: env variable

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV != "prod") require("dotenv").config();
+if (process.env.NODE_ENV != "production") require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
